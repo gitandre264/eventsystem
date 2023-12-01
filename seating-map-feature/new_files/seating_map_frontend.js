9b235c564fff95c55ecea950dc45f74821803995
gitandre264 <138679358+gitandre264@users.noreply.github.com>
2023-12-01 02:09:38 +0100

    Added seating map frontend JavaScript

---
+++ seating-map-feature/new_files/seating_map_frontend.js
@@ -0,0 +1,4 @@
+// Seating Map Frontend JavaScript
+// This file handles the interactive functionality for the seating map.
+
+// Todo : Add functionality to allow organizers to create and edit seating maps and to allow customers to select seats.
